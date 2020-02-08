9d7e8b324977a2d93fc28d8739339d8f8f7d2f7a
Cassie Sloan <cassandra.sloan16@gmail.com>
2020-02-09 09:37:20 +1100

    spun up server and applied two styled componenets to HOME page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import Home from "./components/Home";
+import styled from "styled-components";
 
 
 function App() {
-  return (
-    render()
-  );
+  render() 
+    return(
+      <>
+      <Home/>
+      </>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,28 @@
 import React, {Component} from "react";
+import styled from "styled-components";
+const Title = styled.h1`  
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 class Home extends React.Component {
-    return() {
-        render()
+    render() {
+        return(
+            <>
+            <Wrapper>
+                <Title>
+                    Hello
+                </Title>
+            </Wrapper>
+            </>
+        )
     }
     
 }
+
+export default Home; 
